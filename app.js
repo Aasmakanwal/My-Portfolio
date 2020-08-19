3db131353fc1c1ccca139981d2f4abfa86ebe300
Aasma kanwal <aasmakanwal921@gmail.com>
2020-08-19 15:11:14 +0500

    Mt Portfolio

---
+++ app.js
@@ -0,0 +1,8 @@
+function nevbar(){
+    var sideBar = document.getElementById("Sidebar")
+    sideBar.classList.toggle('active')
+    var bar = document.getElementById("bar")
+    bar.classList.toggle('active')
+    var home = document.getElementById("home")
+    home.classList.toggle('home2')
+}
